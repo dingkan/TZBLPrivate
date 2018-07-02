0af32330f5e1652c2f0daf885b5e5de84ac0d22c
hcsx <657483726@qq.com>
2018-07-02 17:30:46 +0800

    *./ 根据响应连获取控制器

---
+++ TZBLProject/UIView+HCSXTopVc.h
@@ -0,0 +1,13 @@
+//
+//  UIView+HCSXTopVc.h
+//  XCSX
+//
+//  Created by hcsx on 2017/8/23.
+//  Copyright © 2017年 hcsx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (HCSXTopVc)
+- (void)getNav;
+@end
